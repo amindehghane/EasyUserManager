767d5d8b927deb4635e09c23d4c25f88f50f10fe
Amin <aminnahiehdehghan98@gmail.com>
2025-04-03 17:02:49 +0330

    first push

--- src/main/java/amin/easyusermanager/Controller/UserController.java
+++ src/main/java/amin/easyusermanager/Controller/UserController.java
@@ -14,27 +14,36 @@ public class UserController {
 
     @Autowired
     public UserController(UserService userService) {
+
         this.userService = userService;
+
     }
 
     @PostMapping("/create")
     public CreateResponseDto save(@RequestBody CreateRequestDto requestDto) {
+
         return userService.save(requestDto);
+
     }
 
     @GetMapping("/{id}")
     public CreateResponseDto findById(@PathVariable Long id) {
+
         return userService.get(id);
+
     }
 
     @PutMapping("/{id}")
     public CreateResponseDto update(@PathVariable Long id, @RequestBody CreateRequestDto requestDto) {
+
         return userService.update(id, requestDto);
+
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        userService.delete(id);
-    }
 
+        userService.delete(id);
+
+    }
 }
